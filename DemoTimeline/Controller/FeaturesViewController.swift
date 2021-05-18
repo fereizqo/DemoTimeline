835fe3280d33e7fe500598f3dd4629e4220f9a84
Fereizqo Sulaiman <fereizqoahmad@sci.ui.ac.id>
2021-05-18 15:50:18 +0700

    feat: prepare project files

--- DemoTimeline/Controller/FeaturesViewController.swift
+++ DemoTimeline/Controller/FeaturesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FeaturesViewController.swift
 //  DemoTimeline
 //
 //  Created by Fereizqo Sulaiman on 18/05/21.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeaturesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
---
+++ DemoTimeline/Controller/TimelineTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimelineTableViewCell.swift
+//  DemoTimeline
+//
+//  Created by Fereizqo Sulaiman on 18/05/21.
+//
+
+import UIKit
+
+class TimelineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
