469a975bc5cd71a095d9a1aa125b424cd7e7428e
Fereizqo Sulaiman <fereizqoahmad@sci.ui.ac.id>
2021-05-18 15:39:57 +0700

    Initial Commit

---
+++ DemoTimeline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoTimeline
+//
+//  Created by Fereizqo Sulaiman on 18/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
