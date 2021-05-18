fc4f48437a517e5da6f5e1e4cb52898c24e61f2f
Fereizqo Sulaiman <fereizqoahmad@sci.ui.ac.id>
2021-05-18 17:11:12 +0700

    feat: create timeline
    first prototype, still need a lot of improvement

--- DemoTimeline/Controller/TimelineTableViewCell.swift
+++ DemoTimeline/Controller/TimelineTableViewCell.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class TimelineTableViewCell: UITableViewCell {
+    
+    var allRows = Int()
+    var currentIndexPath = IndexPath()
+    let offSet: CGFloat = 20.0
+    let circleRadius: CGFloat = 5.0
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,4 +25,50 @@ class TimelineTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        // Create the circle
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: offSet, y: self.bounds.midY), radius: CGFloat(circleRadius), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        createCircle(path: circlePath)
+        
+        // Create the line
+        let linePath = UIBezierPath()
+        
+        var startPoint = CGPoint()
+        if currentIndexPath.row == 0 {
+            startPoint = CGPoint(x: offSet, y: self.bounds.midY)
+        } else {
+            startPoint = CGPoint(x: offSet, y: 0)
+        }
+        linePath.move(to: startPoint)
+        
+        var endPoint = CGPoint()
+        if currentIndexPath.row == allRows-1 {
+            endPoint = CGPoint(x: offSet, y: self.bounds.maxY)
+        } else {
+            endPoint = CGPoint(x: offSet, y: self.bounds.maxY)
+        }
+        linePath.addLine(to: endPoint)
+        
+        createLine(path: linePath)
+    }
+    
+    func createCircle(path: UIBezierPath) {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.lineWidth = 2.0
+        shapeLayer.fillColor = UIColor.white.cgColor
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        self.layer.addSublayer(shapeLayer)
+    }
+    
+    func createLine(path: UIBezierPath) {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.lineWidth = 2.0
+        shapeLayer.lineCap = .butt
+        shapeLayer.strokeColor = UIColor.systemGray2.cgColor
+        self.layer.insertSublayer(shapeLayer, at: 1)
+    }
 }
--- DemoTimeline/Controller/TimelineTableViewController.swift
+++ DemoTimeline/Controller/TimelineTableViewController.swift
@@ -8,82 +8,73 @@
 import UIKit
 
 class TimelineTableViewController: UITableViewController {
-
+    
+    let testArray = [2020, 2019, 2018]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        let timelineTableViewCellNib = UINib(nibName: "TimelineTableViewCell", bundle: Bundle(for: TimelineTableViewCell.self))
+        self.tableView.register(timelineTableViewCellNib, forCellReuseIdentifier: "TimelineTableViewCell")
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        // return the number of sections
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        // return the number of rows
+        return testArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
+        
+        let label = UILabel()
+        label.frame = CGRect.init(x: 20.0, y: 5, width: headerView.frame.width-10, height: headerView.frame.height-10)
+        label.text = "Title"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        
+        headerView.addSubview(label)
+        
+        let linePath = UIBezierPath()
+        var startPoint = CGPoint()
+        var endPoint = CGPoint()
+        
+        if section == 0 {
+            startPoint = CGPoint(x: 20.0, y: 25)
+        } else {
+            startPoint = CGPoint(x: 20.0, y: 0)
+        }
+        
+        endPoint = CGPoint(x: 20.0, y: 45)
+        linePath.move(to: startPoint)
+        linePath.addLine(to: endPoint)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = linePath.cgPath
+        shapeLayer.lineWidth = 2.0
+        shapeLayer.lineCap = .butt
+        shapeLayer.strokeColor = UIColor.systemGray2.cgColor
+        headerView.layer.insertSublayer(shapeLayer, at: 0)
+        
+        return headerView
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineTableViewCell", for: indexPath) as! TimelineTableViewCell
+        // Configure the cell
+        cell.allRows = testArray.count
+        cell.currentIndexPath = indexPath
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func createLine(path: UIBezierPath) {
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
